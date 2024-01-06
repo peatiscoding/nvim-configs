082f95150d9bb7d5563eac45a6801c23fd665dc6
Kittiphat S <freeuxer@gmail.com>
2024-01-06 14:32:59 +0700

    replace formatter with conform instead.

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -1,5 +1,24 @@
 return {
-  "mhartington/formatter.nvim",
-  config = function()
-  end
+  'stevearc/conform.nvim',
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>f",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+     -- Define your formatters
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { { "prettierd", "prettier" } },
+    },
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+  },
 }
