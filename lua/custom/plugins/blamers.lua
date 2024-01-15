ffaa7dcb603db7c09a5be15da97c09468e372a21
Kittiphat S <freeuxer@gmail.com>
2024-01-15 10:10:18 +0700

    feat: added gitblame

---
+++ lua/custom/plugins/blamers.lua
@@ -0,0 +1,9 @@
+return {
+    'f-person/git-blame.nvim',
+    config = function ()
+        require('gitblame').setup({
+            enabled = true,
+            delay = 300,
+        })
+    end
+}
