fefb42171793f7bf86e5b9efd4fac44a706d11ff
Kittiphat S <freeuxer@gmail.com>
2024-06-28 09:41:16 +0700

    disable codeium for go

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,6 +1,10 @@
 return {
   'Exafunction/codeium.vim',
   config = function()
+    vim.g.codeium_filetypes = {
+      ["*"] = true,
+      ["go"] = false
+    }
     vim.keymap.set('i', '<c-t>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
   end
 }
