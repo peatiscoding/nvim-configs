08c71b6dedeed1d10232ea5e7cc17c7409766399
Kittiphat S <freeuxer@gmail.com>
2025-02-06 13:26:58 +0700

    update terminal to use 92% of screen

--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -1,22 +1,25 @@
 return {
     'akinsho/toggleterm.nvim',
     version = "*",
-    config = function ()
+    config = function()
+        local margin = 0.92
         require('toggleterm').setup {
             open_mapping = [[<c-\>]],
             direction = 'float',
             float_opts = {
-                border  = 'single',
+                border = 'single',
+                width = math.floor(vim.o.columns * margin),
+                height = math.floor(vim.o.lines * margin),
             },
         }
-        local Terminal  = require('toggleterm.terminal').Terminal
-        local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float' })
+        -- Create Terminals
+        local Terminal = require('toggleterm.terminal').Terminal
+        local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = 'float' })
 
         function _LG_TOGGLE()
-          lazygit:toggle()
+            lazygit:toggle()
         end
 
-
-        vim.api.nvim_set_keymap("n", "<C-g>", "<cmd>lua _LG_TOGGLE()<CR>", {noremap = true, silent = true})
+        vim.api.nvim_set_keymap("n", "<C-g>", "<cmd>lua _LG_TOGGLE()<CR>", { noremap = true, silent = true })
     end
 }
