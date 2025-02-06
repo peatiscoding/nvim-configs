152c5312d66fb042cdd655cc369cfd162a343bdc
Kittiphat S <freeuxer@gmail.com>
2025-02-06 16:21:52 +0700

    feat: using devicons on alpha page

--- lua/custom/plugins/alpha.lua
+++ lua/custom/plugins/alpha.lua
@@ -2,6 +2,10 @@ return {
     'goolord/alpha-nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require 'alpha'.setup(require 'alpha.themes.startify'.config)
+        local startify = require('alpha.themes.startify')
+        startify.file_icons.provider = 'devicons'
+        require('alpha').setup(
+            startify.config
+        )
     end
 }
