80603b8645cfdb9a3dd03b89204e8bf69a5421c6
Kittiphat S <freeuxer@gmail.com>
2025-02-06 16:45:07 +0700

    removed mini

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -2,6 +2,8 @@ return {
     'echasnovski/mini.nvim',
     version = '*',
     config = function()
+        require('mini.starter').setup()
+        require('mini.tabline').setup()
         require('mini.bracketed').setup()
     end
 }
