08ecf7572903f80fd683adfc1d62f415726e0865
Kittiphat S <freeuxer@gmail.com>
2025-02-20 08:41:44 +0700

    remove starter, remove tabline

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -2,8 +2,8 @@ return {
     'echasnovski/mini.nvim',
     version = '*',
     config = function()
-        require('mini.starter').setup()
-        require('mini.tabline').setup()
+        -- require('mini.starter').setup()
+        -- require('mini.tabline').setup()
         require('mini.bracketed').setup()
     end
 }
