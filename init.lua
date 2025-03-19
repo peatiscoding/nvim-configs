96c6e8bde80573a71b29bc188f9cdc6aa031981b
Kittiphat S <kittiphat@muze.co.th>
2025-03-19 13:25:58 +0700

    added copy using osc52

--- init.lua
+++ init.lua
@@ -290,6 +290,18 @@ vim.o.mouse = 'a'
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.o.clipboard = 'unnamedplus'
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+  },
+}
+
 
 -- Enable break indent
 vim.o.breakindent = true
